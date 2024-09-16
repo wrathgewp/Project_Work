3bb074cdcc87310cc91eb6f1ec07b9d380c4f9a5
wrathgewp <39569621+wrathgewp@users.noreply.github.com>
2024-09-16 11:37:40 +0200

    Added comments to the few lines of code

--- app.py
+++ app.py
@@ -4,20 +4,26 @@ from dotenv import load_dotenv
 print("Starting bot.")
 
 if os.path.exists(".env"):
+    
+    ## Check if the file .env exists
 
     print("Loading variables from .env file.")
 
-    load_dotenv()
+    load_dotenv() ## Load the env file
 
-    BOT_API = os.environ["BOT_API"]
+    BOT_API = os.environ["BOT_API"] ## Set the BOT_API variable from the env file
 
 elif "BOT_API" in os.environ:
+    
+    ## Check if the BOT_API variable is in the environment variables of the OS / Docker Image
 
     print("No .env file found, using environment variables.")
 
     BOT_API = os.environ["BOT_API"]
 
 else:
+
+    ## Exiting if the BOT_API variable is not set and there is no .env file
     
     print("No .env file found, and no environment variables set. Exiting.")
 
