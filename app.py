4211702f559ff82f867a6ce8131de8a60d7292f3
IFTSbelle <--global>
2024-09-21 12:43:08 +0200

    Added functions echo and unknown

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 import logging
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes
 import pymysql
 from pymysql.err import MySQLError
 
@@ -34,18 +34,37 @@ else:
 
     exit(1)
 
+
+## ---------Telegram API code----------
+
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Ciao sono il bot ifts")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Hi, i am the IFTS ProjectWork bot!")
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(BOT_API).build()
-    
+
     start_handler = CommandHandler('start', start)
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+    unknown_handler = MessageHandler(filters.COMMAND, unknown)
+
     application.add_handler(start_handler)
+    application.add_handler(unknown_handler)
+    application.add_handler(echo_handler)
     
     application.run_polling()
+
+
+
+
+    
