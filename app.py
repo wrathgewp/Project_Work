5c7fe98ab3552262a14acbe0671c0fca91b69669
DeboraPavoni <169908162+DeboraPavoni@users.noreply.github.com>
2024-09-24 09:26:38 +0200

    language buttons added

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import os
 from dotenv import load_dotenv
 import logging
-from telegram import Update
-from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes, Updater, CallbackQueryHandler
 import pymysql
 from pymysql.err import MySQLError
 import sql
@@ -46,6 +46,24 @@ else:
 
 ## ---------Telegram API code----------
 
+
+## Dictionary for the languages
+messages = {
+    "ita": {
+        "welcome": "Benvenutə!",
+        "language_selected": "Lingua italiana impostata, benvenutə!",
+    },
+    "eng": {
+        "welcome": "Welcome!",
+        "language_selected": "English language selected, welcome!",
+    }
+}
+
+## Function to get the user language
+
+def get_user_language(language_code):
+    return "ita" if language_code == "ita" else "eng"
+
 ## The following code activate logging to allow debugging
 
 logging.basicConfig(
@@ -53,10 +71,36 @@ logging.basicConfig(
     level=logging.INFO
 )
 
+user_language = "eng" ##Default language
+
+
 ## The following code will be executed when the bot is started
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Hi, i am the IFTS ProjectWork bot!")
+    ## Buttons creation
+    ita = InlineKeyboardButton("🇮🇹", callback_data='ita')
+    eng = InlineKeyboardButton("🇬🇧", callback_data='eng')
+    
+    ## InlineKeyboardMarkup creation
+    keyboard = [[ita, eng]]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, 
+        text=messages[user_language]["welcome"], 
+        reply_markup=reply_markup
+    )
+    
+## The following code will be executed when the bot receives a callback query (change language)
+
+async def button(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    global user_language
+    query = update.callback_query
+    await query.answer()
+
+    user_language = get_user_language(query.data)
+    
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=messages[user_language]["language_selected"])
 
 ## The following code will be executed when the bot receives an unkown command
 
@@ -69,14 +113,11 @@ if __name__ == '__main__':
     application = ApplicationBuilder().token(BOT_API).build()
 
     start_handler = CommandHandler('start', start)
+    button_handler = CallbackQueryHandler(button)
     unknown_handler = MessageHandler(filters.COMMAND, unknown)
 
     application.add_handler(start_handler)
+    application.add_handler(button_handler)
     application.add_handler(unknown_handler)
     
     application.run_polling()
-
-
-
-
-    
